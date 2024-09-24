66c778d9be45893ceec2452a0a6464aa011b7377
Inheritance <rejwoan.mrp@gmail.com>
2024-09-24 11:08:52 +0600

    done

--- src/Practice1.java
+++ src/Practice1.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Practice1 {
 
@@ -14,7 +17,17 @@ public class Practice1 {
         } catch (IOException e) {
             System.out.println("the error is: " + e.getMessage());
         }
+            try (Scanner scanner = new Scanner(new File("Hello.txt"))){
 
+                while (scanner.hasNextLine()){
+
+                    String line = scanner.nextLine();
+                    System.out.println(line);
+                }
+
+            } catch (FileNotFoundException e) {
+                System.out.println("The error is " + e.getMessage());
+            }
 
     }
 }
